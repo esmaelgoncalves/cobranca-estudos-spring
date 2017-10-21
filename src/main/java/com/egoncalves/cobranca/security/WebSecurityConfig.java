993fbf0a3a586b7564c8b76dd296c3ee50c542d7
Esmael Gonçalves <esmael.goncalves@hotmail.com>
2017-10-21 18:03:33 -0200

    Ajuste

--- src/main/java/com/egoncalves/cobranca/security/WebSecurityConfig.java
+++ src/main/java/com/egoncalves/cobranca/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +18,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             	.antMatchers("/css/**", "/js/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
+    			.csrf().disable()
+    			.httpBasic()
+    			.and()
             .formLogin()
             	.loginPage("/login")
             	.permitAll()
